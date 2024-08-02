eebf45bd09d5fe36dffc8badbb5e8668019526d8
gunnalavanya123 <lavanya.gunna@acelyzei.com>
2024-08-02 10:36:58 +0530

    .

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,15 +1,16 @@
-""" url mappings for the the user api"""
+"""
+URL mappings for the user API.
+"""
+from django.urls import path	
 
-from django.urls import path
+from user import views	
 
 
-from user import views
+app_name='user'	
 
-app_name='user'
+urlpatterns=[	
 
-urlpatterns=[
-    path('create/',views.CreateUserView.as_view,name='create'),
-    path('token/',views.CreateTokenView.as_view(),name='token'),
-   
+    path('create/',views.CreateUserView.as_view,name='create'),	
 
+    path('token/',views.CreateTokenView.as_view(),name='token'),	
 ]
